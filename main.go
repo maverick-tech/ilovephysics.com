cebd550174d524e96f0c6761e2927a1d5f6166a9
maverick-tech <tarlemanmohan@outlook.com>
2020-06-13 19:30:26 +0530

    First Running version of ilovephysics.com

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
-import("fmt")
 
-func main(){
-fmt.Println("Hello,World!")
+import (
+	"fmt"
+	"net/http"
+)
+
+func webHandlerFunc(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "<h1>welcome to I Love Physics !</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", webHandlerFunc)
+	http.ListenAndServe(":3030", nil)
 }
